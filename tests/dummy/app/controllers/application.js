27c14def9d19d2a588feee655b7b516b953fd80e
Jack Matthews <jackmatt2@gmail.com>
2016-01-10 18:18:29 +1100

    remove junk tests

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -6,8 +6,6 @@ import footerTemplate from '../templates/customised/footer';
 import notFoundTemplate from '../templates/customised/not-found';
 import pendingTemplate from '../templates/customised/pending';
 
-const {computed} = Ember;
-
 const countries = Ember.A(["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas",
   "Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands",
   "Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica",
